32174e3649f3810bee201e2380a56802712f2120
tzachariah <tzachari@lmu.edu>
2016-03-21 15:55:40 -0700

    Revert "plot1"
    This reverts commit c40cd07cc0f328db7831ce1fb6c5e0df62a218de.

--- 𝚙𝚕𝚘𝚝𝟷.R
+++
@@ -1,9 +0,0 @@
-##
-## Rcode for Plot1
-##
-> hhpc <- file("household_power_consumption.txt")
-> pc <- read.table(text = grep("^[1,2]/2/2007", readLines(hhpc), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
-> #
-> png(file = "plot1.png")
-hist(pc$Global_active_power, col = "red", main = paste("Global Active Power"), xlab = "Global Active Power (kilowatts)")
->dev.off()
